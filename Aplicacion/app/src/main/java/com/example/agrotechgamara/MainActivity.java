8e0f5340562106c3f8e59b07b4e1e162cf2fcca2
Lucas <Lucasjimnez2004@gmail.com>
2025-11-18 20:22:46 -0300

    cambios

--- Aplicacion/app/src/main/java/com/example/agrotechgamara/MainActivity.java
+++ Aplicacion/app/src/main/java/com/example/agrotechgamara/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.agrotechgamara;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,17 +10,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
+
 
     }
 }
