6c7754b7302fb7d439703d4430292fc893e7b9cd
lucas <lucasjimnez2004@gmail.com>
2025-12-17 08:03:00 -0300

    reg layout

--- Aplicacion/app/src/main/java/com/example/agrotechgamara/ui/fragments/RegistrarseFragment.java
+++ Aplicacion/app/src/main/java/com/example/agrotechgamara/ui/fragments/RegistrarseFragment.java
@@ -2,6 +2,8 @@ package com.example.agrotechgamara.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -32,6 +34,18 @@ public class RegistrarseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registrarse, container, false);
+        View root = inflater.inflate(R.layout.fragment_registrarse, container, false);
+        // Inicializar el ExecutorService solo una vez
+        init(root);
+        return root;
+    }
+
+    private void init(View root) {
+        /*Este codigo hace que la barra de control del celular no se quede por encima de la pantalla o fragmento que estamos mostrando y lo sobre ponga*/
+        ViewCompat.setOnApplyWindowInsetsListener(root, (v, insets) -> {
+            int bottom = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom;
+            v.setPadding(0, 0, 0, bottom);
+            return insets;
+        });
     }
 }
